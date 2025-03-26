0a0514d6d58a409080a77119dcb2dc3c7ce51306
Ivan Oranciuc <95760855+OranciucIvan@users.noreply.github.com>
2025-03-26 19:07:49 +0200

    Update Issue 3.py

--- homework/Ivan_Oranciuc/Homework 7/Issue 3.py
+++ homework/Ivan_Oranciuc/Homework 7/Issue 3.py
@@ -6,14 +6,11 @@ results = ["результат операции: 42",
 
            "результат: 2"]
 
-
 def extract_number(s):
     return int(s.split()[-1])
 
-
 def add_and_print(s):
     print(extract_number(s) + 10)
-
 
 for result in results:
     add_and_print(result)
