0cabed481738a79ffd5a5be275620226e20ef583
OranciucIvan <oranchukivan@gmail.com>
2025-04-06 22:44:02 +0300

    'Issues_for_lesson_9'

---
+++ homework/Ivan_Oranciuc/Homework 9/Issue 2.py
@@ -0,0 +1,15 @@
+from statistics import fmean
+
+temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33,
+                31, 30, 32, 30, 28, 24, 23]
+
+
+def hight_temperatures(temperatures):
+    return temperatures > 28
+
+
+values_for_temperatures = list(filter(hight_temperatures, temperatures))
+
+print(f'Max temperature: {max(values_for_temperatures)}')
+print(f'Min temperature: {min(values_for_temperatures)}')
+print(f'Avg temperature: {round(fmean(values_for_temperatures))}')
